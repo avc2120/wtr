8d5c1c8d4ea966cc6e397ffb086c9cda11da5658
Vishal-N-Vyas <vnv2102@columbia.edu>
2015-11-24 22:58:59 -0500

    K means Clustering Version 1

---
+++ g1/Point.java
@@ -0,0 +1,23 @@
+package wtr.g1;
+
+public class Point {
+
+	@Override
+	public String toString() {
+		return "Point [x=" + x + ", y=" + y + "]";
+	}
+
+	public final double x;
+	public final double y;
+
+	public Point(double x, double y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public double distance(Point p)
+	{
+		return Math.hypot(x - p.x, y - p.y);
+	}
+}
