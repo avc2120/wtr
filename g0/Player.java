6dfb925347e65548d7bde39d079c3f24bfd46f09
Sean Liu <seanliu@dyn-160-39-148-41.dyn.columbia.edu>
2015-12-01 03:36:32 -0500

    restored g0

--- g0/Player.java
+++ g0/Player.java
@@ -4,7 +4,6 @@ import wtr.sim.Point;
 
 import java.io.*;
 import java.util.Random;
-import java.util.Set;
 
 public class Player implements wtr.sim.Player {
 
@@ -13,12 +12,6 @@ public class Player implements wtr.sim.Player {
 
 	// your own id
 	private int self_id = -1;
-	private int soulmate = -1;
-
-	private int last_chat = -1;
-	private int last_wisdom = -1;
-	private int turns_waited = 0;
-	private boolean skip;
 
 	// the remaining wisdom per player
 	private int[] W = null;
@@ -50,83 +43,37 @@ public class Player implements wtr.sim.Player {
 	                  boolean wiser, int more_wisdom)
 	{
 		// find where you are and who you chat with
-		int i = 0;
-		int j = 0;
-		while (players[i].id != self_id) {
-			i++;
-		}
+		int i = 0, j = 0;
+		while (players[i].id != self_id) i++;
+		while (players[j].id != chat_ids[i]) j++;
 		Point self = players[i];
-		while (players[j].id != chat_ids[i]) {
-			j++;
-		}
 		Point chat = players[j];
-
-		if (chat.id != last_chat) {
-      		turns_waited = 0;
-    	}
-
 		// record known wisdom
 		W[chat.id] = more_wisdom;
-		if (W[chat.id] > 50) {
-			soulmate = chat.id;
-		}
-
 		// attempt to continue chatting if there is more wisdom
-		skip = false;
-		if (wiser) {
-			last_chat = chat.id;
-			return new Point(0.0, 0.0, chat.id);
-		} else if (W[chat.id] > 0) {
-			if (++turns_waited > W[chat.id]/5) {
-				skip = true;
-			}
-		}
-
+		if (wiser) return new Point(0.0, 0.0, chat.id);
 		// try to initiate chat if previously not chatting
 		Point m = null;
-		if (i == j || skip) {
-			double best = 0;
+		if (i == j)
 			for (Point p : players) {
 				// skip if no more wisdom to gain
-				if (W[p.id] == 0) {
-					continue;
-				}
-				double temp = score_player(p, players, self);
-				if (temp > best) {
-					best = temp;
-					// start chatting if in range
-					double dd = distance_squared(p, self);
-					if (dd >= 0.25 && dd <= 0.5) {
-						m = new Point(0.0, 0.0, p.id);
-					}
+				if (W[p.id] == 0) continue;
+				// compute squared distance
+				double dx = self.x - p.x;
+				double dy = self.y - p.y;
+				double dd = dx * dx + dy * dy;
+				// start chatting if in range
+				if (dd >= 0.25 && dd <= 4.0) {
+					m = new Point(0.0, 0.0, p.id);
+					break;
 				}
 			}
-		}
-
 		// return a random move if no move yet
 		if (m == null) {
-			boolean r = true;
-			double best = 0;
-			for (Point p : players) {
-				// skip if no more wisdom to gain
-				if (W[p.id] == 0) {
-					continue;
-				}
-				double temp = score_player(p, players, self);
-				if (temp > best) {
-					best = temp;
-					r = false;
-					double x = (p.x - self.x - 0.5);
-					double y = (p.y - self.y - 0.5);
-					m = new Point(x, y, self.id);
-				}
-			}
-			if (r) {
-				double dir = random.nextDouble() * 2 * Math.PI;
-				double dx = 6 * Math.cos(dir);
-				double dy = 6 * Math.sin(dir);
-				m = new Point(dx, dy, self_id);
-			}
+			double dir = random.nextDouble() * 2 * Math.PI;
+			double dx = 6 * Math.cos(dir);
+			double dy = 6 * Math.sin(dir);
+			m = new Point(dx, dy, self_id);
 		}
 		// record move if in debug mode 
 		if (debug) try {
@@ -135,40 +82,5 @@ public class Player implements wtr.sim.Player {
 			stream.close();
 		} catch (IOException e) {}
 		return m;
-	}
-
-	private double score_player(Point p, Point[] players, Point self) {
-		int wisdom = W[p.id];
-		if (p.id == soulmate) {
-			wisdom *= 1.5;
-		}
-		if (has_players_within_radius(p, players, self, 0.5)) {
-			return wisdom - 30;
-		} else if (distance_squared(p, self) < 1) {
-			return wisdom + 10;
-		} else {
-			return wisdom;
-		}
-
-	}
-
-	private double distance_squared(Point a, Point b) {
-		double dx = a.x - b.x;
-		double dy = a.y - b.y;
-		return dx * dx + dy * dy;
-	}
-
-	private boolean has_players_within_radius(Point player, Point[] players, Point self, double r) {
-		for (Point p : players) {
-			if (p == player || p == self)
-				continue;
-			double dx = player.x - p.x;
-			double dy = player.y - p.y;
-			double dd = dx * dx + dy * dy;
-			if (dd <= r) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
